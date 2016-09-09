30e416d28e7730201cbdf582ccf7cbe30c55984c
PascalMunniks1998 <pmunniks@roc-dev.com>
2016-09-09 12:41:08 +0200

    Klasse aanpassing

---
+++ src/H02/H02.java
@@ -0,0 +1,22 @@
+package H02;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by User on 09-09-16.
+ */
+
+
+
+03/02/26
+
+        public class H02 extends Applet {
+
+        public void init() {
+
+        }
+        public void paint(Graphics g) {
+            g.drawString("Welcome to1 Java!!", 50, 60 );
+        }
+}
