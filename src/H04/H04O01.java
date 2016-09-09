6c22f82547e0850bb8ccfab010b66e56f577df6b
PascalMunniks1998 <pmunniks@roc-dev.com>
2016-09-09 13:53:00 +0200

    Driehoek

---
+++ src/H04/H04O01.java
@@ -0,0 +1,20 @@
+package H04;
+
+import java.applet.*;
+import java.awt.*;
+
+/**
+ * Created by User on 09-09-16.
+ */
+public class H04O01 extends Applet {
+
+    public void init() {
+
+    }
+
+    public void paint(Graphics g) {
+      g.drawLine(50,100,100,50);
+      g.drawLine(100,50,150,100);
+      g.drawLine(50,100,150,100);
+    }
+}
