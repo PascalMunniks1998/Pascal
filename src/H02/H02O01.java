ebbe892847cac66c86f5ac0e2ac49491e0569400
PascalMunniks1998 <pmunniks@roc-dev.com>
2016-09-14 09:58:16 +0200

    Oefening Variabelen en Overig

--- src/H02/H02O01.java
+++ src/H02/H02O01.java
@@ -6,18 +6,12 @@ import java.awt.*;
 /**
  * Created by User on 09-09-16.
  */
+public class H02O01 extends Applet {
 
+   public void init() {setBackground(Color.blue);}
 
-
-//03/02/26
-
-        public class H02 extends Applet {
-
-        public void init() {
-          setBackground(Color.blue);
-        }
-        public void paint(Graphics g) {
+   public void paint(Graphics g) {
             g.setColor(Color.yellow);
             g.drawString("Hallo Pascal!!", 50, 60 );
-        }
+   }
 }
