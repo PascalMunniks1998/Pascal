dac568b1c17cf5d2f1cd7ab7824f7956c9038ac1
PascalMunniks1998 <pmunniks@roc-dev.com>
2016-10-26 10:09:13 +0200

    Opdracht 2 van Hoofdstuk 11

---
+++ src/H11/H11O02.java
@@ -0,0 +1,27 @@
+package H11;
+
+import java.applet.Applet;
+import java.awt.*;
+
+/**
+ * Created by ${PascalMunniks} on ${12-10-16).
+ */
+public class H11O02 extends Applet {
+
+
+    @Override
+    public void init() {
+
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        int teller;
+        int y =250;
+        for(teller =10; teller <= 20; teller++) {
+            y -=20;
+            g.drawString("" + teller,50, y);
+
+        }
+    }
+}
