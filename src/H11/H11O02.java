fc38876e511cccaf778694b7d1404c3bf66a8c31
PascalMunniks1998 <pmunniks@roc-dev.com>
2016-10-30 15:44:31 +0100

    Opdracht 2 van Hoofdstuk 11

--- src/H11/H11O02.java
+++ src/H11/H11O02.java
@@ -17,7 +17,7 @@ public class H11O02 extends Applet {
     @Override
     public void paint(Graphics g) {
         int teller;
-        int y = 250;
+        int y = 0;
         for(teller = 10; teller <= 20; teller++) {
             y += 20;
             g.drawString("" + teller,50, y);
