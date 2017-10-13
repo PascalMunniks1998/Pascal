0bed7489745840d551cc8751403a2937fedef5eb
PascalMunniks1998 <pmunniks@roc-dev.com>
2017-10-13 09:53:26 +0200

    Unusual Commit

---
+++ src/Challenges/ChallengeFruitautomaat.java
@@ -0,0 +1,17 @@
+package Challenges;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * Created by Pascal Munniks on 13-10-17.
+ */
+public class ChallengeFruitautomaat extends Applet {
+
+
+    public void init () {
+
+    }
+}
---
+++ src/H14/H14O02.java
@@ -0,0 +1,83 @@
+package H14;
+
+import java.applet.Applet;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+public class H14O02 extends Applet{
+
+    private String[] typen = {"klaver", "schoppen", "harten", "ruiten"};
+    private String[] kaarten = {"aas", "2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "heer"};
+    private String[] deck = new String[52];
+    private String[] speler1;
+    private String[] speler2;
+    private String[] speler3;
+    private String[] speler4;
+    Button knop;
+    int k;
+
+    boolean geklikt;
+
+    public void init() {
+        knop = new Button("Deel kaart");
+        knop.addActionListener(new KnopListener());
+        add(knop);
+
+        int index = 0;
+        for (int i = 0; i < typen.length; i++) {
+            String soort = typen[i];
+            for (int j = 0; j < kaarten.length; j++) {
+                String nummer = kaarten[j];
+                deck[index] = soort + " " + nummer;
+                index++;
+            }
+        }
+    }
+
+    private String deelKaart() {
+        int random = new Random().nextInt(deck.length);
+        String kaart = deck[random];
+
+        String[] hulpLijst = new String[deck.length - 1];
+        int hulpindex = 0;
+        for (int i = 0; i < deck.length; i++) {
+            if (i != random) {
+                hulpLijst[hulpindex] = deck[i];
+                hulpindex++;
+            }
+        }
+        deck = hulpLijst;
+        return kaart;
+    }
+
+    private void spel() {
+        for (int index = 0; index < 13; index++) {
+            speler1[index] = deelKaart();
+            speler2[index] = deelKaart();
+            speler3[index] = deelKaart();
+            speler4[index] = deelKaart();
+        }
+    }
+
+
+    private class KnopListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            Random kaart = new Random();
+            int r = kaart.nextInt(52);
+            k = r;
+            geklikt = true;
+            repaint();
+        }
+    }
+    public void paint(Graphics g) {
+        knop.setBackground(Color.RED);
+        knop.setForeground(Color.WHITE);
+
+        if (geklikt == true) {
+            g.drawString("" + deck[k], 50, 60);
+        }
+    }
+}
