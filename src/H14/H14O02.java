bd4c8730e19c34d530388d1c867560b7ad62a159
PascalMunniks1998 <pmunniks@roc-dev.com>
2017-10-17 11:05:26 +0200

    Opdracht 14.2 AF

--- src/H14/H14O02.java
+++ src/H14/H14O02.java
@@ -8,14 +8,19 @@ import java.util.Random;
 
 public class H14O02 extends Applet{
 
+    //kaarten
     private String[] typen = {"klaver", "schoppen", "harten", "ruiten"};
     private String[] kaarten = {"aas", "2", "3", "4", "5", "6", "7", "8", "9", "10", "boer", "vrouw", "heer"};
     private String[] deck = new String[52];
-    private String[] speler1;
-    private String[] speler2;
-    private String[] speler3;
-    private String[] speler4;
-    Button knop;
+
+    //spelers
+    private String[] speler1 = new String[13];
+    private String[] speler2 = new String[13];
+    private String[] speler3 = new String[13];
+    private String[] speler4 = new String[14];
+
+    
+    private Button knop;
     int k;
 
     boolean geklikt;
@@ -40,35 +45,44 @@ public class H14O02 extends Applet{
         int random = new Random().nextInt(deck.length);
         String kaart = deck[random];
 
-        String[] hulpLijst = new String[deck.length - 1];
-        int hulpindex = 0;
+        String[] deck1 = new String[deck.length - 1];
+        int index = 0;
         for (int i = 0; i < deck.length; i++) {
             if (i != random) {
-                hulpLijst[hulpindex] = deck[i];
-                hulpindex++;
+                deck1[index] = deck[i];
+                index++;
             }
         }
-        deck = hulpLijst;
+        deck = deck1;
         return kaart;
-    }
-
-    private void spel() {
-        for (int index = 0; index < 13; index++) {
-            speler1[index] = deelKaart();
-            speler2[index] = deelKaart();
-            speler3[index] = deelKaart();
-            speler4[index] = deelKaart();
-        }
     }
 
 
     private class KnopListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            Random kaart = new Random();
-            int r = kaart.nextInt(52);
-            k = r;
+            setSize(750, 425);
             geklikt = true;
+            deck = new String[52];
+            int index = 0;
+
+            for (int i = 0; i < typen.length; i++) {
+                String kleur = typen[i];
+                for (int j = 0; j < kaarten.length; j++) {
+                    String kaart = kaarten[j];
+                    deck[index] = kleur + " " + kaart;
+                    index++;
+                }
+            }
+
+            for (int i = 0; i < 13; i++) {
+                speler1[i] = deelKaart();
+                speler2[i] = deelKaart();
+                speler3[i] = deelKaart();
+                speler4[i] = deelKaart();
+            }
+
+
             repaint();
         }
     }
@@ -76,8 +90,25 @@ public class H14O02 extends Applet{
         knop.setBackground(Color.RED);
         knop.setForeground(Color.WHITE);
 
-        if (geklikt == true) {
-            g.drawString("" + deck[k], 50, 60);
+        int x1 = 50;
+        int x2 = 250;
+        int x3 = 450;
+        int x4 = 650;
+        int y = 70;
+        int y1 = 105;
+
+        if (geklikt) {
+            g.drawString("Speler 1", x1, y);
+            g.drawString("Speler 2", x2, y);
+            g.drawString("Speler 3", x3, y);
+            g.drawString("Speler 4", x4, y);
+            for (int i = 0; i < 13; i++) {
+                g.drawString(speler1[i], x1, y1);
+                g.drawString(speler2[i], x2, y1);
+                g.drawString(speler3[i], x3, y1);
+                g.drawString(speler4[i], x4, y1);
+                y1 += 25;
+            }
         }
     }
 }
